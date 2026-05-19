aac642e013d6c7e2011f4cf024f90805065eaf5f
Setoju <setoju48@gmail.com>
2026-05-19 12:36:30 +0300

    test(commit): verify auto-split creates grouped commit history

---
+++ spec/lib/flows/commit_flow_auto_split_integration_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'tmpdir'
+require 'open3'
+require 'fileutils'
+
+RSpec.describe 'CommitFlow auto-split', :integration do
+  class FakeGoogleClient
+    def initialize
+      @counter = 0
+    end
+
+    def generate(**_kwargs)
+      @counter += 1
+      "feat: grouped commit #{@counter}"
+    end
+  end
+
+  def git!(dir, *args)
+    out, err, status = Open3.capture3('git', *args, chdir: dir)
+    raise "git #{args.join(' ')} failed: #{err.strip.empty? ? out.strip : err.strip}" unless status.success?
+
+    out
+  end
+
+  it 'creates multiple commits with grouped files for connected changes' do
+    Dir.mktmpdir('commiti-auto-split') do |dir|
+      git!(dir, 'init')
+      git!(dir, 'config', 'user.email', 'commiti-test@example.com')
+      git!(dir, 'config', 'user.name', 'Commiti Test')
+
+      FileUtils.mkdir_p(File.join(dir, 'lib/services'))
+      FileUtils.mkdir_p(File.join(dir, 'spec/lib/services'))
+
+      File.write(File.join(dir, 'lib/services/message_generator.rb'), "module M\nend\n")
+      File.write(File.join(dir, 'spec/lib/services/message_generator_spec.rb'), "RSpec.describe M do\nend\n")
+      File.write(File.join(dir, 'README.md'), "# Commiti\n")
+
+      git!(dir, 'add', '-A')
+
+      allow(Commiti::Spinner).to receive(:run) { |_message, &block| block.call }
+      allow(Commiti::GoogleClient).to receive(:new).and_return(FakeGoogleClient.new)
+      allow(Commiti::InteractivePrompt).to receive(:ask_yes_no).and_return(false)
+      allow(Commiti::InteractivePrompt).to receive(:ask_commit_action).and_return(:yes)
+
+      Dir.chdir(dir) do
+        flow = Commiti::Flows::CommitFlow.new(options: { auto_split: true, no_copy: true, candidates: 1 })
+        flow.run
+      end
+
+      commit_count = git!(dir, 'rev-list', '--count', 'HEAD').strip.to_i
+      expect(commit_count).to eq(2)
+
+      hashes = git!(dir, 'rev-list', '--reverse', 'HEAD').lines.map(&:strip)
+
+      changed_files_by_commit = hashes.map do |sha|
+        git!(dir, 'show', '--name-only', '--pretty=format:', sha)
+          .lines
+          .map(&:strip)
+          .reject(&:empty?)
+          .sort
+      end
+
+      expect(changed_files_by_commit).to match_array([
+                                                       ['README.md'],
+                                                       ['lib/services/message_generator.rb', 'spec/lib/services/message_generator_spec.rb']
+                                                     ])
+    end
+  end
+end
